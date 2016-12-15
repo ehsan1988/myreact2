5218307cea562002e588c82a62c42c081f4f27b5
ehsan <birnaghdejavan@gmail.com>
2016-12-15 10:14:56 +0330

    update server.js for heroku

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express=require('express');
 var app=express();
-app.use(express.static('public'));
+const PORT=process.env.PORT||3000;
+app.use(function (req,res,next) {
+    if (req.headers['x-forwarded-proto']==='http'){
+       next();
+    }
+    else {
+       res.redirect('http://'+req.hostname+req.url);
+    }
+});
 app.listen(3000,function () {
    console.log('server run in port 3000');
 });
